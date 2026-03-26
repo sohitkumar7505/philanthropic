504e7a23300e1cffc6ef3a68c13d49bd2d5cd4cf
sohitkumar7505 <sohitkumar3139@gmail.com>
2026-03-26 13:41:32 +0530

    vercel link

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://philanthropic-lotter.onrender.com/api",
 });
 
 // attach token
